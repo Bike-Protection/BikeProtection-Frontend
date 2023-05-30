488402a4f88d4fb9c8c9f59900c03fa493bcb50f
SolomiaSydoryk <sydoryk213@gmail.com>
2023-05-30 15:34:33 +0300

    added the current view of the application

---
+++ screens/MainScreen.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Button, ImageBackground, Text } from 'react-native';
+import { THEME } from '../src/Theme';
+
+const MainScreen = ({navigation}) => {
+    const localImage = require('../assets/fon3.png');
+    const [changeColor, SetChangeColor] = useState(false);
+    const [blocked, SetBlocked] = useState(false);
+
+    const onPressHandler = () => {
+      SetBlocked(!blocked);
+      SetChangeColor(!changeColor);
+    }
+
+    return (
+        <View style = {{flex: 1}}>
+         <View >
+         <ImageBackground source={localImage} style={styles.image}/>
+         <View style={styles.eclipse}/>
+         
+            <View style={styles.viewText}>
+             {blocked ?
+             <Text style={styles.text}>
+              Пристрій заблоковано
+             </Text>
+             :
+             <Text style={styles.text}>
+              Пристрій розблоковано
+             </Text>
+            }
+
+            </View>
+            <View style={styles.buttons}> 
+            <Button 
+            title={blocked ? 'Розблокувати' : 'Заблокувати'}
+            onPress={onPressHandler}
+            color = {blocked ? THEME.GREEN_COLOR : THEME.RED_COLOR}
+            />
+          </View>
+         </View>
+        </View>
+    );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+   image: {
+        flex: 1,
+        height: 880,
+        width: '100%'
+    }, 
+    buttons: {
+        marginTop: 180,
+        paddingHorizontal: 90
+    },
+    eclipse: {
+        backgroundColor: 'rgba(0, 0, 0, 0.65)',
+        width: '94%',
+        height: 545,
+        position: 'absolute', 
+        borderRadius: 20,
+        left: '3%',
+        right: '3%',
+        top: '15%',
+    },
+    viewText: {
+        marginTop: '80%',
+        alignItems: 'center', 
+    },
+    text: {
+        color: 'white',
+        fontSize: 20
+    }
+});
